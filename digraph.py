e3789ae4c07a29615abcd6f74537929da61981f0
edutra <ebodutra@gmail.com>
2018-07-03 23:12:00 -0300

    transformUnGraph(not implemented yet)

--- digraph.py
+++ digraph.py
@@ -11,11 +11,11 @@
     def __init__(self):
         self.graph = []
 
-
+    #insert a relation between graphs
     def insertRelation(self, x, y, z):
         relation = (x,y,z)
         self.graph.append(relation)
-
+    #Transform a digraph into a undirected graph
     #def transformUnGraph(g):
 
 
