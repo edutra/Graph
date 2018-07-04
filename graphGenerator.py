158edd7d010a32c661fe6449b12d1d541c0e9578
edutra <ebodutra@gmail.com>
2018-07-03 22:39:26 -0300

    graphGenerator

---
+++ graphGenerator.py
@@ -0,0 +1,22 @@
+#####################################################
+#
+# Coded by: Enrique B. Dutra
+# In : 07/03/18
+#
+#####################################################
+import random
+numberNodes = int(input())
+print(numberNodes)
+
+
+for x in range(numberNodes):
+    print(nodeName(x) + "      " + random.randint(0, x))
+
+
+
+
+
+def nomeName(x):
+
+        
+
