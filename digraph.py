3a7f8df5c6dcd5e67ca2e246c149b494dd69a426
edutra <ebodutra@gmail.com>
2018-07-03 23:06:22 -0300

    graph deleted. digraph created

---
+++ digraph.py
@@ -0,0 +1,20 @@
+#####################################################
+#
+# Coded by: Enrique B. Dutra
+# In : 07/03/18
+#
+#####################################################
+
+#[(from, edge, to)]
+
+
+    def __init__(self):
+        self.graph = []
+
+
+    def insertRelation(self, x, y, z):
+        relation = (x,y,z)
+        self.graph.append(relation)
+
+
+
